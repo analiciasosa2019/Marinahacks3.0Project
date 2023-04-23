66f4e495cc9e1a466b305589c31f75950f4d3625
CarlaZ488 <66750350+CarlaZ488@users.noreply.github.com>
2023-04-23 08:45:24 -0700

    summary page blank

--- components/summaryPage.js
+++ components/summaryPage.js
@@ -1,41 +1,69 @@
-app.component('summaryPage',{
-    template: 
+app.component('summaryPage', {
+    template:
     /*html*/
     `
-    <div class = "title">{{pageTitle}}</div>
-    <div class = "summaryOuterBox">
-        <div class = "graphArea">
-            <div class = "spiderChartArea">
+    <div class="title">{{pageTitle}}</div>
+    <div class="summaryOuterBox">
+        <div class="graphArea">
+            <div class="spiderChartArea">
                 <spider-chart></spider-chart>
             </div>
-            <div class = "lineGraphArea">
+            <div class="lineGraphArea">
                 <barChart></barChart>
             </div>
         </div>
-        <div class = "studentListArea">
-            <div class = "studentInfo"></div>
-            <div class = "studentList">
-
+        
+        <div class="studentListArea">
+            <div class="studentInfo">
+                <div v-if="selectedStudent">
+                <h2>{{ selectedStudent.name }}</h2>
+                <!-- Display other student information here -->
+                </div>
+            </div>
+            
+            <div class="studentList">
+                <ul>
+                <li
+                    v-for="student in sortedStudentList"
+                    :key="student.id"
+                    @click="selectStudent(student)"
+                    :class="{ 'highlighted': selectedStudent && selectedStudent.id === student.id }"
+                >
+                    {{ student.name }}
+                </li>
+                </ul>
             </div>
         </div>
         
     </div>
     `,
-    data(){
-        return{
-            pageTitle: "Student Summary"
-        }
-    }, 
-    props:{
-        studentList: Array,
+    props: {
+      pageTitle: {
+        type: String,
+        required: true,
+      },
+      studentList: {
+        type: Array,
+        required: true,
+      },
     },
-    method:{
-        emitStudent(){
-            obj = {
-                
-            }
-            window.mitt.on("studentData",obj)
-        }
+    data() {
+      return {
+        selectedStudent: null,
+      };
     },
-
-})
+    computed: {
+      sortedStudentList() {
+        return [...this.studentList].sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+      },
+    },
+    methods: {
+      selectStudent(student) {
+        this.selectedStudent = student;
+        this.$emit('student-selected', student);
+      },
+    },
+  });
+  
