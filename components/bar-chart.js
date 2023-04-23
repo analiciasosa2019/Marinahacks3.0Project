63a4bae7c4a802ebfea2832ba23690491cf542bf
Analicia Sosa <analiciasosa2019@gmail.com>
2023-04-23 07:15:47 -0700

    created barChart.js

---
+++ components/bar-chart.js
@@ -0,0 +1,86 @@
+app.component('barChart', {
+    template:
+      /*html*/
+      `
+      <canvas ref="barChart"></canvas>
+      `,
+    data() {
+      return {
+        test: 'spider-chart',
+        barData: {math: {2022: 78, 2021: 76, 2020: 80},
+        english: {2022: 85, 2021: 82, 2020: 79},
+        science : {2022: 82, 2021: 77, 2020: 84},
+        history: {2022: 80, 2021: 78, 2020: 73}}
+      };
+    },
+    methods:{          
+          // extract values into list
+          years: Object.keys(this.barData.math),
+          valuesByYear: years.map(year => {
+            const values = {};
+            for (const key in grades) {
+              values[key] = grades[key][year];
+            }
+            return values;
+          })
+    },
+
+    mounted() {
+        window.mitt.on('Studentdata', (avgData) => {
+        console.log("mitt recieved!", avgData)
+        })
+      const barcanvas = this.$refs.barChart;    
+        const chartbar = new Chart(barcanvas, {
+            type: 'line', // Specify the chart type
+            data: {
+              labels: 
+                Object.keys(this.barData.math)
+              , // Specify the labels for the axes
+              datasets: [
+                {
+                  label: Object.values(this.barData.math), // Specify the legend label for the dataset
+                  data: Object.values(this.barData.math), // Specify the data for the axes
+                  fill: false, // Specify that the area under the line should be filled
+                  backgroundColor: 'rgba(255, 99, 132, 0.2)', // Specify the background color for the area
+                  borderColor: 'rgba(255, 99, 132, 1)', // Specify the border color for the area
+                  borderWidth: 1 // Specify the border width for the area
+                },
+                {
+                  label: Object.values(this.barData), // Specify the legend label for the dataset
+                  data: [85, 70, 95, 80, 75, 80], // Specify the data for the axes
+                  fill: false, // Specify that the area under the line should be filled
+                  backgroundColor: 'rgba(255, 99, 132, 0.2)', // Specify the background color for the area
+                  borderColor: 'rgba(255, 99, 132, 1)', // Specify the border color for the area
+                  borderWidth: 1 // Specify the border width for the area
+                },            
+                {
+                  label: Object.values(this.barData), // Specify the legend label for the dataset
+                  data: [85, 70, 95, 80, 75, 80], // Specify the data for the axes
+                  fill: false, // Specify that the area under the line should be filled
+                  backgroundColor: 'rgba(255, 99, 132, 0.2)', // Specify the background color for the area
+                  borderColor: 'rgba(255, 99, 132, 1)', // Specify the border color for the area
+                  borderWidth: 1 // Specify the border width for the area
+                },            
+                {
+                  label: Object.values(this.barData), // Specify the legend label for the dataset
+                  data: [85, 70, 95, 80, 75, 80], // Specify the data for the axes
+                  fill: false, // Specify that the area under the line should be filled
+                  backgroundColor: 'rgba(255, 99, 132, 0.2)', // Specify the background color for the area
+                  borderColor: 'rgba(255, 99, 132, 1)', // Specify the border color for the area
+                  borderWidth: 1 // Specify the border width for the area
+                }
+              ]
+            },
+            options: {
+              scales: {
+                y: {
+                  suggestedMin: 10, // Specify the minimum value for the axes
+                  suggestedMax: 100 // Specify the maximum value for the axes
+                }
+              }
+            }
+          });
+    }
+  });
+
+  
